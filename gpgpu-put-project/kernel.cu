41986b3fe0a2f98aa8f3a0d625c9f0d9da42d465
jax9000 <jax1994@gmail.com>
2019-07-04 09:27:12 +0200

    Begin impl kernel

--- gpgpu-put-project/kernel.cu
+++ gpgpu-put-project/kernel.cu
@@ -9,8 +9,8 @@
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 
-#define SEQ_SIZE 9
-#define PATTERN_SIZE 4
+
+#define BLOCK_SIZE 1024
 
 
 __global__ void addKernel(int *c, const int *a, const int *b)
@@ -58,15 +58,26 @@ unsigned long long variations_without_repetitions_count(int n, int k) {
 	return result;
 }
 
-//https://www.cs.put.poznan.pl/wandrzejewski/wp-content/uploads/gpgpu/ZZKG2019_06_Sekwencje_kombinatoryczne.pdf
-// trzeba chyba usuwac elementy z tablicy xD (A \= A[x])
-void variation(int n, int k, int variationNumber, int* result) {
+__device__ void variations_without_repetitions_count_dev(int n, int k, unsigned long long* result) {
+	if (k > n) {
+		*result = 1;
+		return;
+	}
+
+	*result = 1;
+	for (int i = n; i > n - k; i--) {
+		*result *= i;
+	}
+}
+
+__device__ void variation(int n, int k, int variationNumber, int* result) {
 	bool* isTaken = new bool[n];
 	for (int i = 0; i < n; i++) {
 		isTaken[i] = false;
 	}
 	for (int x = 0; x < k ; x++) {
-		unsigned long long v = variations_without_repetitions_count(n - x - 1, k - x - 1);
+		unsigned long long v = 0;
+		variations_without_repetitions_count_dev(n - x - 1, k - x - 1, &v);
 		auto t = variationNumber / v;
 		int searchedPosition = -1;
 		int realPosition = 0;
@@ -86,27 +97,78 @@ void variation(int n, int k, int variationNumber, int* result) {
 	}
 }
 
+
+__global__ void findSubstitution(
+	char* patternValues, int patternValuesSize, 
+	int* seqValues, int seqValuesSize, 
+	char* pattern, int patternSize,
+	int* seq, int seqSize,
+	int* result, unsigned long long variationCount) {
+
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	if (index > variationCount) return;
+	
+	int* variationResult = new int[patternValuesSize];
+	variation(seqValuesSize, patternValuesSize, index, variationResult);
+}
+
 int main()
 {  
 	// 124353621
     //const int seq[SEQ_SIZE] = { 1,2, 4, 3, 5, 3, 6, 2, 1 };
     //const char pattern[PATTERN_SIZE] = { 'a', 'b', 'b', 'a' };
 	std::vector<int> seq = { 1,2, 4, 3, 5, 3, 6, 2, 1 };
-	std::vector<char> pattern = { 'a', 'b', 'b', 'a', 'c' };
+	std::vector<char> pattern = { 'a', 'b', 'b', 'a' };
 
 	thrust::host_vector<char>* patternValues = getHostVector(getUniqueValues(&pattern));
+	thrust::host_vector<char>* thrustPattern = getHostVector(&pattern);
 	thrust::host_vector<int>* seqValues = getHostVector(getUniqueValues(&seq));
+	thrust::host_vector<int>* thrustSeq = getHostVector(&seq);
 
-	for (int i = 0; i < variations_without_repetitions_count(seqValues->size(), patternValues->size()); i++) {
-		int* result = new int[patternValues->size()];
-		variation(seqValues->size(), patternValues->size(), i, result);
-		for (int j = 0; j < patternValues->size(); j++) {
-			//std::cout << (*seqValues)[result[j]];
-			std::cout << result[j];
-		}
-		std::cout << std::endl;
+	thrust::device_vector<char>* devPatternValues = new thrust::device_vector<char>();
+	thrust::device_vector<char>* devThrustPattern = new thrust::device_vector<char>();
+	thrust::device_vector<int>* devSeqValues = new thrust::device_vector<int>();
+	thrust::device_vector<int>* devThrustSeq = new thrust::device_vector<int>();
+
+	//for (int i = 0; i < variations_without_repetitions_count(seqValues->size(), patternValues->size()); i++) {
+	//	int* result = new int[patternValues->size()];
+	//	variation(seqValues->size(), patternValues->size(), i, result);
+	//	for (int j = 0; j < patternValues->size(); j++) {
+	//		//std::cout << (*seqValues)[result[j]];
+	//		std::cout << result[j];
+	//	}
+	//	std::cout << std::endl;
+	//}
+
+	unsigned long long variationCount = variations_without_repetitions_count(seqValues->size(), patternValues->size());
+	int gridSize = variationCount / BLOCK_SIZE;
+	if (gridSize < 1) {
+		gridSize = 1;
 	}
+	int* result = new int[variationCount];
 
+	devPatternValues->resize(patternValues->size());
+	devThrustPattern->resize(thrustPattern->size());
+	devSeqValues->resize(seqValues->size());
+	devThrustSeq->resize(thrustSeq->size());
+
+	*devPatternValues = *patternValues;
+	*devThrustPattern = *thrustPattern;
+	*devSeqValues = *seqValues;
+	*devThrustSeq = *thrustSeq;
+
+	findSubstitution <<< gridSize, BLOCK_SIZE >>> (
+		devPatternValues->data().get(), devPatternValues->size(),
+		devSeqValues->data().get(), devSeqValues->size(),
+		devThrustPattern->data().get(), devThrustPattern->size(),
+		devThrustSeq->data().get(), devThrustSeq->size(),
+		result, variationCount);
+
+	cudaError_t err = cudaGetLastError();
+	if (err != cudaSuccess) {
+		std::cout << "cuda error: " << cudaGetErrorString(err) << std::endl;
+		return 1;
+	}
 
 	//thrust::device_vector<char>* devicePatternValues = new thrust::device_vector<char>();
 	//thrust::device_vector<int>* deviceSeqValues = new thrust::device_vector<int>();
